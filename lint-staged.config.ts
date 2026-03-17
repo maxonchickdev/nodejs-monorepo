65553ff4beded03b8faab379a65d4a2c56158b7d
maxonchickdev <timetotime828@gmail.com>
2026-03-17 12:43:39 +0200

    chore(root): update includes to exclude 'dist' in backend and web configurations; enhance lint-staged checks

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,22 +1,26 @@
-import { type Configuration } from "lint-staged";
+import type { Configuration } from "lint-staged";
 
 const config: Configuration = {
 	"*": [
-		(): string => "npm run lint:editor:check",
 		(): string => "npm run lint:clean:check",
 		(): string => "npm run lint:filesystem:check",
+		(): string => "npm run lint:format:check",
+		(): string => "npm run lint:check",
 	],
 	"apps/backend/**/*.ts": [
 		(): string => "npm run lint:check -w apps/backend",
 		(): string => "npm run lint:format:check -w apps/backend",
+		(): string => "npm run lint:types:check -w apps/backend",
 	],
 	"apps/web/**/*.ts": [
 		(): string => "npm run lint:check -w apps/web",
 		(): string => "npm run lint:format:check -w apps/web",
+		(): string => "npm run lint:types:check -w apps/web",
 	],
 	"packages/shared/**/*.ts": [
 		(): string => "npm run lint:check -w packages/shared",
 		(): string => "npm run lint:format:check -w packages/shared",
+		(): string => "npm run lint:types:check -w packages/shared",
 	],
 };
 
